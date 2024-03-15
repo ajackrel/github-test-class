4ef6647ae646e135a45c0af96ff62e2100812f4a
26nbhatt <26nbhatt@stratfordschools.net>
2024-03-15 11:24:40 -0700

    just a commit

--- app/src/main/java/com/example/githubtestclass/MainActivity.java
+++ app/src/main/java/com/example/githubtestclass/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("This is a print message -computer #1");
+        System.out.println("This is a MUCH better print message");
 
     }
 }
